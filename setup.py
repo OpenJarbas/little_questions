c00917239391610893181226216661341ee714ab
jarbasal <jarbasai@mailfence.com>
2019-03-12 16:21:01 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import setup
+import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('little_questions')
 
 setup(
     name='little_questions',
@@ -6,6 +18,9 @@ setup(
     packages=['little_questions', 'little_questions.data',
               'little_questions.utils', 'little_questions.parsers'],
     url='https://github.com/JarbasAl/little_questions',
+    install_requires=['numpy', 'scikit-learn', 'spacy', 'padaos', 'gensim'],
+    package_data={'': extra_files},
+    include_package_data=True,
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
