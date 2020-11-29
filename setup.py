04bee86244b42fdaed9f8d010c2f83037ad753f6
jarbasal <jarbasai@mailfence.com>
2020-11-29 03:25:33 +0000

    pip release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.6.0',
     packages=['little_questions', 'little_questions.data',
               'little_questions.utils', 'little_questions.parsers',
-              'little_questions.classifiers'],
+              'little_questions.classifiers', 'little_questions.features'],
     url='https://github.com/OpenJarbas/little_questions',
     install_requires=['numpy==1.16.1', 'scikit-learn', 'gensim', 'padaos',
                       "nltk", 'simple_NER>=0.6.1', "fann2==1.0.7",
